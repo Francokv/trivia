8489e6b0636d1b1b2f781bb1c25761d1b0c53ee6
Franco Vallejos <fvallejosx@gmail.com>
2024-08-05 16:31:46 -0400

    chore: Remove ssr option from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ["vuetify-nuxt-module", "@prisma/nuxt"],
-  ssr: true,
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
     treeShake: true,
