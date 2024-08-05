505528ae33a33451135323757b98fdcd4b168463
Franco Vallejos <fvallejosx@gmail.com>
2024-08-05 16:44:53 -0400

    chore: Remove @prisma/nuxt module from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["vuetify-nuxt-module", "@prisma/nuxt"],
+  modules: ["vuetify-nuxt-module"],
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
     treeShake: true,
